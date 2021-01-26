6c21c694fc0b3b48b583df7cddf54782a4c475a2
trevoramann <trevor.amann9@gmail.com>
2021-01-26 00:33:01 -0600

    Commented the rest of the program

--- Assignments/A03/main.cpp
+++ Assignments/A03/main.cpp
@@ -2,31 +2,55 @@
 //                   
 // Author:           Trevor Amann
 // Email:            trevor.amann9@gmail.com
-// Label:            (program's label from assignment list)
-// Title:            (short title from assignment, if any)
-// Course:           (course number and prefix)
-// Semester:         (semester and year)
+// Label:            A03
+// Title:            Basic Program Organization - Commenting
+// Course:           CMPS 2143
+// Semester:         Spring 2021
 //
 // Description:
-//       describe program here thoroughly 
+//    The program we have created here is a circular array queue. The program pushes
+//    and pops integers from a queue.
 //
 // Usage:
-//       how to use the program if necessary
+//       To use this program you must tell it what to push, pop, and when to print the list out. 
 //
-// Files:            (list of all source files used in this program)
+// Files:   main.cpp
 /////////////////////////////////////////////////////////////////////////////////
-
-
-
 
 
 #include <iostream>
 
 using namespace std;
 
-/* In this section we are declaring a class called CircularArrayQue. 
-* In the private section we initialize class variables and a function called init that we can call later.
-*/
+/**
+ * CircularArrayQue
+ * 
+ * Description:
+ *      This class implements a compressions algorithm called CircularArrayQue Coding.
+ *      CircularArrayQue coding assigns codes to characters such that the length of the 
+ *      code depends on the relative frequency or weight of the corresponding 
+ *      character   CircularArrayQue codes are of variable-length, and prefix-free
+ * 
+ * Public Methods:
+ *                          CircularArrayQue()
+ *                          CircularArrayQue(int size)    
+ *      void                Push(int item)
+ *      int                 Pop()
+ *      friend              ostream &operator 
+ * 
+ * Private Methods:
+ *      void                init()
+ *      bool                Full()
+ *
+ * 
+ * Usage: 
+ * 
+ *      Must call your push and pop functions in the main and tell it what integers to push and when to pop. 
+ *      Also tell it whent to print which is what the ostream is for.  
+ *      
+ */
+
+
 class CircularArrayQue {
 private:
     int *Container;
@@ -46,19 +70,55 @@ private:
 // in the public section of this class we declare all of our functions that we use in the future to run our program. 
 
 public:
-    //This is the constructor for our class and it initializes the object which is our array or queue. 
+    /**
+     * Public : CircularArrayQue
+     * 
+     * Description:
+     *      The default constructor that intializes our queue. 
+     * 
+     * Params:
+     *      Container   : queue
+     *      int[10]     : queue size
+     * 
+     * Returns:
+     *      Nothing.
+     */
     CircularArrayQue() {
         Container = new int[10];
         init(10);
     }
 
-    //This is the second constructor of our class but this one is the parameterized constructor. 
+    /**
+     * Public : CircularArrayQue(int size)
+     * 
+     * Description:
+     *      Defines a queue of whatever size we choose in main. 
+     * 
+     * Params:
+     *      Container   : queue with size we declare in main.
+     *      int[size]   : queue size
+     * 
+     * Returns:
+     *      Nothing.
+     */
     CircularArrayQue(int size) {
         Container = new int[size];
         init(size);
     }
 
-    //Below is our push function that we can call to add new items to our queue. 
+    /**
+     * Public : Push(int item)
+     * 
+     * Description:
+     *      Pushes an integer onto the queue.
+     * 
+     * Params:
+     *      item    :  integer we are pushing
+     *      Rear    :  the rear of the queue
+     * 
+     * Returns:
+     *      FULL if we push too many integers onto the queue.
+     */
     void Push(int item) {
         if (!Full()) {
             Container[Rear] = item;
@@ -69,7 +129,21 @@ public:
         }
     }
 
-    //Below is the pop function that we call when we want to pop whatever integer is at the back of the queue off of the queue. 
+    /**
+     * Public : Pop
+     * 
+     * Description:
+     *      Removes an integer from the queue. 
+     * 
+     * Params:
+     *      temp        :  a temporary slot that we use
+     *      Front       : The front of our queue
+     *      CurrentSize : the current size of the queue
+     *      QueSize     : the size of the queue that we declared
+     * 
+     * Returns:
+     *      List*   : a pointer to a linked list of integers.
+     */
     int Pop() {
         int temp = Container[Front];
         Front = (Front + 1) % QueSize;
@@ -81,7 +155,20 @@ public:
     friend ostream &operator<<(ostream &os, const CircularArrayQue &other);
 };
 
-//This is our overloading function that we use to traverse the queue and then print out our end result after we go through our main function. 
+/**
+     * ostream &operator 
+     * 
+     * Description:
+     *      Prints the queue after pushing and popping
+     * 
+     * Params:
+     *      other.Front       : calls the front of our queue
+     *      other.CurrentSize : calls the current size of the queue
+     *      other.QueSize     : calls the size of the queue that we declared
+     * 
+     * Returns:
+     *      List*   : a pointer to a linked list of integers.
+     */
 ostream &operator<<(ostream &os, const CircularArrayQue &other) {
 
     for (int i = other.Front; i < other.CurrentSize; i = (i + 1) % other.QueSize) {
@@ -98,7 +185,7 @@ ostream &operator<<(ostream &os, const CircularArrayQue &other) {
  * 
  */
 int main() {
-    CircularArrayQue C1(5);
+    CircularArrayQue C1(5); //Declared the queue to be size 5 and named C1.
 
     // C1.Push(34);
     // C1.Push(38);
@@ -107,16 +194,16 @@ int main() {
     // C1.Push(99);
     // C1.Push(100);
 
-    C1.Push(1);
-    C1.Push(2);
-    C1.Push(3);
+    C1.Push(1);          //Pushed the integer 1 onto the queue.
+    C1.Push(2);          //Pushed the integer 2 onto the queue.
+    C1.Push(3);          //Pushed the integer 3 onto the queue.
     // C1.Push(6);
     // C1.Push(7);
-    cout << C1 << endl;
+    cout << C1 << endl;  //Printed the queue. 
 
     // C1.Push(1);
     // C1.Push(2);
     // C1.Push(3);
 
-    cout << C1 << endl;
+    cout << C1 << endl;  //Printed the queue again. 
 }
